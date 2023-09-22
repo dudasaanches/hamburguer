1215fc7b9befd2b990f9cbeba10b8465259d5ac8
Maria Eduarda Sanches <124930071+dudasaanches@users.noreply.github.com>
2023-09-22 16:19:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 var banner = document.getElementById("bannerH")
 var bannerc = document.getElementById("bannerC")
-var combo = document.getElementById("combo")
 
 
 
-function excluirlanches(){
-    var lanche1 = document.getElementById("produtos")
-    lanche1.style.display = "none"
-}
+bannerc.addEventListener("click", function(){
+    var lanche1 = document.getElementById("produtos-1")
+    var lanche2 = document.getElementById("produtos-2")
+    var combo = document.getElementById("combo")
 
-bannerc.addEventListener("submit", excluirlanches)
+    combo.style.display = "flex"
+    
+    if(lanche1.style.display === "none"){
+        lanche1.style.display = "flex"
+        lanche2.style.display = "flex"
+    }
+    else{
+        lanche1.style.display = "none"
+        lanche2.style.display = "none"
+    }
+});
+
+banner.addEventListener("click", function(){
+    var combo = document.getElementById("combo")
+    var lanche1 = document.getElementById("produtos-1")
+    var lanche2 = document.getElementById("produtos-2")
+
+    lanche1.style.display = "flex"
+    lanche2.style.display = "flex"
+    
+    if(combo.style.display === "none"){
+        combo.style.display = "flex"
+    }
+    else{
+        combo.style.display = "none"
+    }
+});
